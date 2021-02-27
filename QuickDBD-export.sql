9d7ed8fc7c40e7f3fee1a331910840ebaf5f2574
Austin Keeler <71201527+austink24@users.noreply.github.com>
2021-02-26 23:22:16 -0500

    Updating Schemma

--- QuickDBD-export.sql
+++ QuickDBD-export.sql
@@ -2,31 +2,8 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Player" (
-    "player_id" varchar   NOT NULL,
-    "name" varchar   NOT NULL,
-    "age" int   NOT NULL,
-    "DOB" date   NOT NULL,
-    "weight" int   NOT NULL,
-    "height" int   NOT NULL,
-    "team_name" vachar   NOT NULL,
-    CONSTRAINT "pk_Player" PRIMARY KEY (
-        "player_id"
-     )
-);
-
-CREATE TABLE "Teams" (
-    "games_played" int   NOT NULL,
-    "advancd_postseason" varchar   NOT NULL,
-    "salary" currency   NOT NULL,
-    "team_name" varchar   NOT NULL,
-    CONSTRAINT "pk_Teams" PRIMARY KEY (
-        "team_name"
-     )
-);
-
 CREATE TABLE "Player_Stats" (
-    "player_id" varchar   NOT NULL,
+    "player_id" int   NOT NULL,
     "Games_Played" int   NOT NULL,
     "hits_at_bat" int   NOT NULL,
     "seasons_played" int   NOT NULL,
@@ -39,12 +16,37 @@ CREATE TABLE "Player_Stats" (
      )
 );
 
-ALTER TABLE "Player" ADD CONSTRAINT "fk_Player_player_id" FOREIGN KEY("player_id")
-REFERENCES "Player_Stats" ("");
+CREATE TABLE "Player" (
+    "player_id" int   NOT NULL,
+    "name" varchar   NOT NULL,
+    "age" int   NOT NULL,
+    "DOB" date   NOT NULL,
+    "weight" int   NOT NULL,
+    "height" int   NOT NULL,
+    "team_name" varchar   NOT NULL,
+    "teamId" varchar   NOT NULL,
+    CONSTRAINT "pk_Player" PRIMARY KEY (
+        "player_id"
+     ),
+    CONSTRAINT "uc_Player_teamId" UNIQUE (
+        "teamId"
+    )
+);
 
-ALTER TABLE "Teams" ADD CONSTRAINT "fk_Teams_team_name" FOREIGN KEY("team_name")
-REFERENCES "Player_Stats" ("");
+CREATE TABLE "Teams" (
+    "games_played" int   NOT NULL,
+    "advancd_postseason" varchar   NOT NULL,
+    "salary" int   NOT NULL,
+    "team_name" varchar   NOT NULL,
+    "teamId" varchar   NOT NULL,
+    CONSTRAINT "pk_Teams" PRIMARY KEY (
+        "teamId"
+     )
+);
 
 ALTER TABLE "Player_Stats" ADD CONSTRAINT "fk_Player_Stats_player_id" FOREIGN KEY("player_id")
-REFERENCES "Player" ("");
+REFERENCES "Player" ("player_id");
+
+ALTER TABLE "Teams" ADD CONSTRAINT "fk_Teams_teamId" FOREIGN KEY("teamId")
+REFERENCES "Player" ("teamId");
 
